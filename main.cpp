35171cbf4d022fe7d8e2c94dbb1959e16d270a4c
Francisco Antunes Carriço <F.M.AntunesCarrico@student.tudelft.nl>
2025-12-25 19:47:59 +0100

    begin opengl

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#include <glad/glad.h>
+
 #include "common.hpp"
+
+#include <GLFW/glfw3.h>
 
 #include "camera.cpp"
 #include "obj.cpp"
@@ -8,7 +12,90 @@
 
 using namespace glm;
 
-int main(int, char **) {
+std::string load_file(const std::string &path) {
+    std::ifstream file(path);
+    std::stringstream ss;
+    ss << file.rdbuf();
+    return ss.str();
+}
+
+GLuint compile_shader(GLenum type, const std::string &path) {
+    std::string src = load_file(path);
+    const char *csrc = src.c_str();
+
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &csrc, nullptr);
+    glCompileShader(shader);
+
+    int ok;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &ok);
+    if (!ok) {
+        char log[1024];
+        glGetShaderInfoLog(shader, 1024, nullptr, log);
+
+        std::cerr << "Shader error:\n" << log << std::endl;
+    }
+
+    return shader;
+}
+
+void init() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window =
+        glfwCreateWindow(WIDTH, HEIGHT, "Raytracer", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    float verts[] = {-1.f, -1.f, 3.f, -1.f, -1.f, 3.f};
+
+    GLuint vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+
+    GLuint vs = compile_shader(GL_VERTEX_SHADER, "shaders/fullscreen.vert");
+    GLuint fs = compile_shader(GL_FRAGMENT_SHADER, "shaders/raytrace.frag");
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    while (!glfwWindowShouldClose(window)) {
+        int w, h;
+        glfwGetFramebufferSize(window, &w, &h);
+        glViewport(0, 0, w, h);
+
+        glUseProgram(program);
+        glUniform2f(glGetUniformLocation(program, "resolution"), w, h);
+
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
+
+int main() { init(); }
+
+void main_old(int, char **) {
 
     HitList world;
 
